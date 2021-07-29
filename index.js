17923b9d7e9df0350fd6913b2f885e20255b1eee
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-07-29 16:17:55 -0400

    commit tree 60874d8cb8c3488ca1f906858566a4e0d67dd82a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory';
+newHeader.innerHTML = "D'Angelo is the champion"
+document.body.appendChild(newHeader);
+
